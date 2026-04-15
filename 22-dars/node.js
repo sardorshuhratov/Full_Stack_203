f315cfbdf1298368394c585f5ba4dc7bb4a130ba
sardorshuhratov <sardorbekshuhratov709@gmail.com>
2026-04-15 16:40:43 +0500

    express

--- 22-dars/node.js
+++ 22-dars/node.js
@@ -1,1 +1,24 @@
-console.log("salom node.js");
+const fs = require('fs')
+
+fs.readFile('./file.txt', 'utf-8', (err, data) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(data);
+});
+
+// fs.writeFile('./file.txt', 'Men file ichiga yozayabman...', (err) => {
+//     if (err) {
+//         console.log(err)
+//         return;
+//     }
+// })
+
+// fs.appendFile('./file.txt', 'Men buni davomidan yozayabman', (err) => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+// })
+
