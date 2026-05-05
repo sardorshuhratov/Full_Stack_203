852a2f42e46bf22336ec16765fdd624d4d9d8e80
sardorshuhratov <sardorbekshuhratov709@gmail.com>
2026-05-05 10:48:59 +0500

    Fix auth routes and token config

--- 29-dars_Autentifikatsiya/controllers/authControllers.js
+++ 29-dars_Autentifikatsiya/controllers/authControllers.js
@@ -1,10 +1,11 @@
 const bcrypt = require("bcrypt");
 const { readUsers, writeUsers } = require('../models/userModels');
+const { generateToken } = require('../libs/token');
 
 function register(req, res) {
   try {
     const { full_name, email, age, password } = req.body;
-    console.log("User data:", { full_name, email, age, password });
+    // console.log("User data:", { full_name, email, age, password });
     if (!full_name || !email || !age || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
@@ -28,6 +29,30 @@ function register(req, res) {
   }
 }
 
+function login(req, res) {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "Maydonni to'ldiring" });
+    } 
+    let users = readUsers();
+    let user = users.find(user => user.email === email);
+    if (!user) {
+      return res.status(404).json({ message: "Bu email royxatdan o'tmagan" });
+    }
+    let isPasswordValid = bcrypt.compareSync(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Parol xato" });
+    } 
+
+    let token = generateToken(user);
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+  
+}
+
 module.exports = {
-  register
+  register, login
 }
--- 29-dars_Autentifikatsiya/routes/authRoutes.js
+++ 29-dars_Autentifikatsiya/routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { register } = require('../controllers/authControllers');
+const { register, login } = require('../controllers/authControllers');
 const router = express.Router();
 
 
 router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;
